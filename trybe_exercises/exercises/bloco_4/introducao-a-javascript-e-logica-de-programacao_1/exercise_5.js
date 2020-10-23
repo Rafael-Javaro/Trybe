5ef764ca32702978cc8ad9801a4ee4e4781e9e8b
Rafael <rafaeljavaro750@gmail.com>
2020-10-23 20:11:33 -0300

    Resolvendo Ex. 5

---
+++ trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/exercise_5.js
@@ -0,0 +1,12 @@
+let x;
+let y;
+let z;
+
+function angulosDoTriangulo (x,y,z) {
+    result = x + y + z;
+    if (result === 180) {
+        console.log(true);
+    } else {
+        console.log(false);
+    };
+};
