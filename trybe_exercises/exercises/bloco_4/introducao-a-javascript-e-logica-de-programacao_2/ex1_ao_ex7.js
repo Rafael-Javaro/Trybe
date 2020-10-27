5a3bcfc16d0daf7528fb52496d48f1f7678c4e3f
Rafael <rafaeljavaro750@gmail.com>
2020-10-27 14:44:37 -0300

    Respondendo Ex.1

--- trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/ex1_ao_ex7.js
+++ trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/ex1_ao_ex7.js
@@ -1,7 +1,9 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /*
 1.Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();*/
-
+for (let i = 0; i < numbers.length; i += 1){
+    console.log(numbers[i]);
+};
 /*2.Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;*/
 
 /*3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
