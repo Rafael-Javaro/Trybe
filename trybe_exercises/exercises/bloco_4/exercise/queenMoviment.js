4cf7deb4685fe9774592d189600e4de1d6298713
Rafael <rafaeljavaro750@gmail.com>
2020-10-27 17:34:36 -0300

    Verifica horizontal e superior Direito

--- trybe_exercises/exercises/bloco_4/exercise/queenMoviment.js
+++ trybe_exercises/exercises/bloco_4/exercise/queenMoviment.js
@@ -4,12 +4,25 @@ let posicaoRainhaY = 4;
 
 //Posição da Peça    
 let posicaoPecaX = 4;
-let posicaoPecaY = 7;
+let posicaoPecaY = 8;
 
 //Verifica se o ataque pode ocorrer
 let ataqueBemSucedido = false;
 
-if(posicaoRainhaX === posicaoPecaX || posicaoRainhaY === posicaoPecaY){
+if(posicaoRainhaX == posicaoPecaX || posicaoRainhaY == posicaoPecaY){
     ataqueBemSucedido = true;
+}
+//Diagonal Superior Direita
+for(let supDir = 1; supDir < 8; supDir += 1){
+    let rainhaLinha = posicaoRainhaX + supDir;
+    let rainhaColuna = posicaoRainhaY + supDir;
+
+    if (rainhaLinha > 8 || rainhaColuna > 8){
+        break;
+    }
+
+    if ((posicaoPecaX == rainhaLinha) && (posicaoPecaY == rainhaColuna)){
+        ataqueBemSucedido = true;
+    }
 }
 console.log(ataqueBemSucedido);
