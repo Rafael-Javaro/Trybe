7dbd5389a60822317c9a09a513b718edf7a96027
Rafael <rafaeljavaro750@gmail.com>
2020-10-27 14:59:23 -0300

    Resolvendo Ex. 3

--- trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/ex1_ao_ex7.js
+++ trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/ex1_ao_ex7.js
@@ -16,6 +16,14 @@ console.log(sum);
 
 /*3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.*/
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let sum = 0;
+for (i = 0; i < numbers.length; i += 1){
+    sum += numbers[i];
+}
+console.log(numbers.length);
+console.log(sum);
+console.log(sum / numbers.length);
 
 /*4.Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";*/
 
