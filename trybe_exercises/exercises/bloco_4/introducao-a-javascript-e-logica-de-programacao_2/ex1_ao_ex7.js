9da4c701341dbfa163b8770616a2c80bf5fae37e
Rafael <rafaeljavaro750@gmail.com>
2020-10-27 15:50:11 -0300

    Resolvendo Ex 4

--- trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/ex1_ao_ex7.js
+++ trybe_exercises/exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/ex1_ao_ex7.js
@@ -1,33 +1,42 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 /*
 1.Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();*/
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let i = 0; i < numbers.length; i += 1){
-    console.log(numbers[i]);
+
+    console.log("Resposta Ex1: "+ numbers[i]);
 };
 
 /*2.Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;*/
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let sum = 0;
 for (i = 0; i < numbers.length; i += 1){
     sum += numbers[i];
 }
-console.log(sum);
+console.log("Resposta Ex2: " + sum);
 
 
 /*3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.*/
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+sum = 0
 for (i = 0; i < numbers.length; i += 1){
     sum += numbers[i];
 }
-console.log(numbers.length);
-console.log(sum);
-console.log(sum / numbers.length);
+console.log("Resposta Ex3: " + sum / numbers.length);
 
 /*4.Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";*/
+sum = 0;
+for (i = 0; i < numbers.length; i += 1){
+    sum += numbers[i];
+} if (sum / numbers.length > 20){
+    console.log("Resposta Ex4: " + "valor maior que 20");
+} else {
+    console.log("Resposta Ex4: " + "valor menor ou igual a 20");
+};
 
 /*5.Utilizando for, descubra qual o maior valor contido no array e imprima-o;*/
+let biggestNumber = Math.max(numbers);
+console.log(biggestNumber);
+
+
 
 /*6.Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";*/
 
