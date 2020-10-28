a751590a8e0a9879ce9f8967f519844742765ec1
Rafael <rafaeljavaro750@gmail.com>
2020-10-28 19:32:01 -0300

    Parte 1, Ex2

--- trybe_exercises/exercises/bloco_4/exercises_4.4/part1_ex1.js
+++ trybe_exercises/exercises/bloco_4/exercises_4.4/part1_ex1.js
@@ -7,4 +7,21 @@ let info = {
   /*1.Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 Valor esperado no console: Bem-vinda, Margarida*/
 
-console.log("Bem-vinda, " + info.personagem);
+console.log("Bem-vinda, " + info.personagem + "!");
+
+console.log("---------------------");
+
+/*2.nsira no objeto uma nova propriedade com o nome de chave "recorrente" e o valor "Sim" e, em seguida, imprima o objeto no console.
+Valor Esperado:
+                       {
+ personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+}
+*/
+info.recorrente = "Sim";
+
+console.log(info);
+
+console.log("---------------------");
