320a88ad31a169fe770feeef4c0d0e01898de228
Rafael <rafaeljavaro750@gmail.com>
2020-10-28 21:28:03 -0300

    Parte 1 - Ex1 ao Ex7

--- trybe_exercises/exercises/bloco_4/exercises_4.4/part1_ex1_ao_ex5.js
+++ trybe_exercises/exercises/bloco_4/exercises_4.4/part1_ex1_ao_ex5.js
@@ -55,3 +55,24 @@ for (key in info){
     console.log(info[key]);
 }
 console.log("------------------------");
+
+/* 5.Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+Valore Esperado:
+Margarida e Tio Patinhas
+Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+Ambos recorrentes // Atenção para essa última linha!
+
+*/
+
+let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain , Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+};
+
+
+for (key in info, info2){
+    console.log(info[key],"e " + info2[key]);
+}
