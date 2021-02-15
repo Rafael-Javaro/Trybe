1c118b8ba530f83c087bbf774f0971889e5107ca
Rafael <rafaeljavaro750@gmail.com>
2021-02-15 20:31:05 -0300

    Refatora

--- trybe_exercises/exercises/Front-End/testando-meu-computador/src/components/ReactClass.jsx
+++ trybe_exercises/exercises/Front-End/testando-meu-computador/src/components/ReactClass.jsx
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 export default class ReactClass extends Component {
   render() {
     return (
-        <h1>My First React Class Component!</h1>
+        <>
+          <h1>My First React Class Component!</h1>
+          <span className="HelloWorld">Hello World!</span>
+        </>
     );
   }
 }
