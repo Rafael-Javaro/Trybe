6012ece3092c84f5347d4f4f96a36c9abb997435
Rafael <rafaeljavaro750@gmail.com>
2021-02-15 19:45:44 -0300

    Reinicia

--- trybe_exercises/exercises/Front-End/testando-meu-computador/src/App.js
+++ trybe_exercises/exercises/Front-End/testando-meu-computador/src/App.js
@@ -1,18 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  function tick() {
-    const element = (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      </div>
-    );
-
-    
-  }
-  
-  setInterval(tick, 1000);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
