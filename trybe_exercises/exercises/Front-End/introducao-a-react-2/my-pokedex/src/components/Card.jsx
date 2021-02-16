ed37032d638e07e72eb6fa21eb9e3634c52c65ea
Rafael <rafaeljavaro750@gmail.com>
2021-02-16 13:09:56 -0300

    Imprime Pokedex na Tela

--- trybe_exercises/exercises/Front-End/introducao-a-react-2/my-pokedex/src/components/Card.jsx
+++ trybe_exercises/exercises/Front-End/introducao-a-react-2/my-pokedex/src/components/Card.jsx
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 
 export default class Card extends Component {
   render() {
-    return (<span>My Pokemon Card</span>);
-    }
-}
-
-Card.propTypes = {
-    name: PropTypes.string.isRequired,
-    type: PropTypes.string.isRequired,
-    averageWeight: PropTypes.shape({
-        value: PropTypes.number,
-        measurementUnit: PropTypes.string,
-    }).isRequired
+    const { name, type, averageWeight, image } = this.props;
+    return (
+        <div className="card">
+            <img alt={`${name}-card`} src={image} />
+            <h2>{name}</h2>
+            <h3>{type}</h3>
+            <p>{`Average Weight: ${averageWeight.value} ${averageWeight.measurementUnit}`}</p>
+        </div>
+    );
+  }
 }
--- trybe_exercises/exercises/Front-End/introducao-a-react-2/my-pokedex/src/components/Pokedex.jsx
+++ trybe_exercises/exercises/Front-End/introducao-a-react-2/my-pokedex/src/components/Pokedex.jsx
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import Card from './Card';
+import data from '../data'
 
 export default class Pokedex extends Component {
+  constructor() {
+    super();
+
+    this.props = data;
+  };
+
   render() {
-    return (<span>{<Card />}</span>);    
+    return (
+      <div>
+        {data.map(pokemon => <Card key={pokemon.id} image={pokemon.image} name={pokemon.name} type={pokemon.type} averageWeight={pokemon.averageWeight} />)}
+      </div>
+    );    
   }
 }
