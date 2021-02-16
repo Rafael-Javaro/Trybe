743da1d891bacb4ec27d7e84b31f329d1b097520
Rafael <rafaeljavaro750@gmail.com>
2021-02-15 21:10:38 -0300

    Exercicios 1 e 2

--- trybe_exercises/exercises/Front-End/introducao-a-react-1/exercicios_bloco_11_introducao-a-react/src/App.js
+++ trybe_exercises/exercises/Front-End/introducao-a-react-1/exercicios_bloco_11_introducao-a-react/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
+const goodRotine = ['Sleep Well', 'Eat Health Food', 'Drink Water', 'Cardio Sessions is Always important', 'Live', 'Repeat', '...']
+
+const task = goodRotine.map((value) => 
+  <li>{value}</li>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {task}
     </div>
   );
 }
